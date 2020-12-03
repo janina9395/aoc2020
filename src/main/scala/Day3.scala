8c2256d1bb4f77ffc81419e847c23366f1bb5c3e
ianina <yanina.km@gmail.com>
2020-12-03 18:20:20 +0100

    Refactor Day3

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -1,33 +1,42 @@
 import scala.io.Source
 
+class Grid(val input: Seq[Array[Char]]) {
+  private val height = input.length
+  private val width = input.head.length
+
+  private var curX = 0
+  private var curY = 0
+
+  def move(stepX: Int, stepY: Int): Unit = {
+    curX = curX + stepX
+    curY = curY + stepY
+  }
+
+  def hasTree: Boolean = input(curY)(curX % width) == '#'
+
+  def canMoveDown: Boolean = curY < height
+}
+
 object Day3 extends App {
 
   def readFile(filename: String): Seq[Array[Char]] =
-    Source.fromResource(filename).getLines.map {line => line.trim.toCharArray}.toSeq
+    Source.fromResource(filename).getLines.map(_.trim.toCharArray).toSeq
 
-  def countTrees(input: Seq[Array[Char]], startX: Int, startY: Int): Int = {
-    val height = input.length;
-    val width = input(0).length
-
-    var curX = startX
-    var curY = startY
-
+  def countTrees(input: Seq[Array[Char]], stepX: Int, stepY: Int): Int = {
+    val grid = new Grid(input)
     var count = 0
-    while (curY < height) {
-      if (input(curY)(curX) == '#') {
+    while (grid.canMoveDown) {
+      if (grid.hasTree) {
         count = count + 1
       }
-      curX = curX + startX
-      if (curX >= width)
-        curX = curX - width
-      curY = curY + startY
+      grid.move(stepX, stepY)
     }
     count
   }
 
   def treesOnSlopes(slopes: Seq[(Int, Int)], input: Seq[Array[Char]]): Long = {
-    slopes.map {
-      case (x, y) => countTrees(input, x, y).toLong
+    slopes.map { case (x, y) =>
+      countTrees(input, x, y).toLong
     }.product
   }
 
@@ -37,7 +46,7 @@ object Day3 extends App {
   val input = readFile("input_day3")
   println(s"Part1: ${countTrees(input, 3, 1)}")
 
-  val slopes = Seq((1,1), (3,1), (5,1), (7,1), (1,2))
+  val slopes = Seq((1, 1), (3, 1), (5, 1), (7, 1), (1, 2))
   assert(treesOnSlopes(slopes, sample) == 336)
 
   println(s"Part2: ${treesOnSlopes(slopes, input)}")
