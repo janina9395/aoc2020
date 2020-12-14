7861cbc8e18d4857c36592d6623f50c168bd2e31
ianina <yanina.km@gmail.com>
2020-12-14 20:16:43 +0100

    Day 14 Refactoring

--- src/main/scala/Day14.scala
+++ src/main/scala/Day14.scala
@@ -1,42 +1,72 @@
-import scala.collection.mutable
 import scala.io.Source
+
+object StringImplicits {
+  implicit class StringBinaryOps(s: String) {
+
+    def leftPadTo(length: Int, ch: Char): String = {
+      "".padTo(length - s.length, ch) + s
+    }
+
+    def toLongRadix(radix: Int): Long = {
+      java.lang.Long.valueOf(s, radix)
+    }
+
+    def toIntRadix(radix: Int): Int = {
+      Integer.valueOf(s, radix)
+    }
+  }
+}
 
 case class Instruction(index: Int, value: Int, mask: Array[Char])
 
 object Instruction {
-
-  def binaryStringWithLeadingZeros(number: Int): String =
-    f"${number.toBinaryString}%36s".replace(' ', '0')
+  import StringImplicits._
 
   def applyMask(value: Int, mask: Array[Char]): Long = {
-    java.lang.Long.valueOf(binaryStringWithLeadingZeros(value).toCharArray.zip(mask).map {
-      case (n, 'X') => n
-      case (_, m) => m
-    }.mkString(""), 2)
+    value.toBinaryString
+      .leftPadTo(36, '0')
+      .toCharArray
+      .zip(mask)
+      .map {
+        case (n, 'X') => n
+        case (_, m)   => m
+      }
+      .mkString("")
+      .toLongRadix(2)
   }
 
   def applyMaskToAddress(value: Int, mask: Array[Char]): Seq[Long] = {
 
-    def replaceX(in: String, repl: String): Seq[String] = {
-      if (!in.contains('X')) {
-        Seq(in)
-      } else {
-        val updated = in.replaceFirst("X", repl)
-        replaceX(updated, "0") ++ replaceX(updated, "1")
+    def allXReplaced(s: String): Seq[String] = {
+      def replaceX(in: String, replacement: String): Seq[String] = {
+        if (!in.contains('X')) {
+          Seq(in)
+        } else {
+          val updated = in.replaceFirst("X", replacement)
+          replaceX(updated, "0") ++ replaceX(updated, "1")
+        }
       }
+
+      replaceX(s, "0") ++ replaceX(s, "1")
     }
 
-    val s = binaryStringWithLeadingZeros(value).toCharArray.zip(mask).map {
-      case (_, 'X') => 'X'
-      case (n, '0') => n
-      case (_, '1') => '1'
-    }.mkString("")
+    val s = value.toBinaryString
+      .leftPadTo(36, '0')
+      .toCharArray
+      .zip(mask)
+      .map {
+        case (_, 'X') => 'X'
+        case (n, '0') => n
+        case (_, '1') => '1'
+      }
+      .mkString("")
 
-    (replaceX(s, "0") ++ replaceX(s, "1")).map(java.lang.Long.valueOf(_, 2))
+    allXReplaced(s).map(_.toLongRadix(2))
   }
 }
 
 object Day14 extends App {
+  import Instruction._
 
   private def readFile(filename: String): Seq[Instruction] = {
     val maskRegex = """mask = (\w+)""".r
@@ -48,31 +78,34 @@ object Day14 extends App {
       .getLines
       .toSeq
       .flatMap {
-      case maskRegex(n) =>
-        mask = n.toCharArray
-        None
-      case memRegex(index, value) =>
-        Some(Instruction(index.toInt, value.toInt, mask))
-    }
-
-  }
-
-  def part1(input: Seq[Instruction]) = {
-    var res = mutable.Map[Int, Long]()
-    input.foreach { instr =>
-      res += instr.index -> Instruction.applyMask(instr.value, instr.mask)
-    }
-    res.values.sum
-  }
-
-  def part2(input: Seq[Instruction]) = {
-    var res = mutable.Map[Long, Long]()
-    input.foreach { instr =>
-      Instruction.applyMaskToAddress(instr.index, instr.mask).foreach { address =>
-        res += address -> instr.value.toLong
+        case maskRegex(n) =>
+          mask = n.toCharArray
+          None
+        case memRegex(index, value) =>
+          Some(Instruction(index.toInt, value.toInt, mask))
       }
-    }
-    res.values.sum
+  }
+
+  def part1(input: Seq[Instruction]): Long = {
+    input
+      .map { instr =>
+        instr.index -> applyMask(instr.value, instr.mask)
+      }
+      .toMap
+      .values
+      .sum
+  }
+
+  def part2(input: Seq[Instruction]): Long = {
+    input
+      .flatMap { instr =>
+        applyMaskToAddress(instr.index, instr.mask).map { address =>
+          address -> instr.value.toLong
+        }
+      }
+      .toMap
+      .values
+      .sum
   }
 
   val sample = readFile("sample_day14")
