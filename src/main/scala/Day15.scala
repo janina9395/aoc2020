ec6387671310d834424267dc9f53c03feb092882
ianina <yanina.km@gmail.com>
2020-12-15 20:20:06 +0100

    Day 15: add recursive version

--- src/main/scala/Day15.scala
+++ src/main/scala/Day15.scala
@@ -1,3 +1,5 @@
+import scala.annotation.tailrec
+
 object Day15 extends App {
 
   def lastSpoken(in: Seq[Int], n: Int = 2020): Int = {
@@ -14,16 +16,34 @@ object Day15 extends App {
     lastNum
   }
 
-  assert(lastSpoken(Seq(0, 3, 6)) == 436)
-  assert(lastSpoken(Seq(1, 3, 2)) == 1)
-  assert(lastSpoken(Seq(2, 1, 3)) == 10)
-  assert(lastSpoken(Seq(1, 2, 3)) == 27)
-  assert(lastSpoken(Seq(2, 3, 1)) == 78)
-  assert(lastSpoken(Seq(3, 2, 1)) == 438)
-  assert(lastSpoken(Seq(3, 1, 2)) == 1836)
+  // Recursive version to compare performance
+  def lastSpokenRec(in: Seq[Int], n: Int = 2020): Int = {
 
-  println(s"Part1: ${lastSpoken(Seq(2, 20, 0, 4, 1, 17))}")
+    @tailrec
+    def f(lastnum: Int, index: Int, numberIndex: Map[Int, Int]): Int = {
+      if (index == n) {
+        lastnum
+      } else {
+        val nextLastNum =
+          numberIndex.get(lastnum).map(index - 1 - _).getOrElse(0)
+        f(nextLastNum, index + 1, numberIndex + (lastnum -> (index - 1)))
+      }
+    }
 
-  //assert(lastSpoken(Seq(0, 3, 6), 30000000) == 175594)
-  println(s"Part2: ${lastSpoken(Seq(2, 20, 0, 4, 1, 17), 30000000)}")
+    f(in.last, in.length, in.slice(0, in.length - 1).zipWithIndex.toMap)
+  }
+
+  assert(lastSpokenRec(Seq(0, 3, 6)) == 436)
+  assert(lastSpokenRec(Seq(1, 3, 2)) == 1)
+  assert(lastSpokenRec(Seq(2, 1, 3)) == 10)
+  assert(lastSpokenRec(Seq(1, 2, 3)) == 27)
+  assert(lastSpokenRec(Seq(2, 3, 1)) == 78)
+  assert(lastSpokenRec(Seq(3, 2, 1)) == 438)
+  assert(lastSpokenRec(Seq(3, 1, 2)) == 1836)
+
+  println(s"Part1: ${lastSpokenRec(Seq(2, 20, 0, 4, 1, 17))}")
+
+  //assert(LastSlokenRec
+  //(Seq(0, 3, 6), 30000000) == 175594)
+  println(s"Part2: ${lastSpokenRec(Seq(2, 20, 0, 4, 1, 17), 30000000)}")
 }
